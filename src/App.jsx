08a2f8374145eb40777b7c98ea6bc6250040dad7
PankajS2002 <109162571+PankajS2002@users.noreply.github.com>
2022-12-31 18:59:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header"
+import Meme from "./Meme"
+import "./index.css"
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./index.css"
+
+
+export default function Header() {
+    return (
+        <header className="header">
+           
+            <h2 className="header--title">Meme Generator</h2>
+            
+        </header>
+    )
+}
